b81e69137d74b3625f05122b5385aa8702358fe0
wangqishan <645210441@qq.com>
2019-08-18 12:10:18 +0800

    ok

---
+++ smartbus-db/src/main/java/com/pcis/smartbus/db/dao/UserManualMapper.java
@@ -0,0 +1,27 @@
+package com.pcis.smartbus.db.dao;
+
+import com.pcis.smartbus.db.domain.SmartbusUser;
+
+import java.util.List;
+
+// this file wrote by hand, differ from auto generated
+//dao下*Mapper.java 与 *Mapper.xml必须一致，否则找不到映射路径
+public interface UserManualMapper {
+    //This is wrote by hand.
+    SmartbusUser selectByUserName(String userName);
+
+    Integer getUserNum();
+
+    List<SmartbusUser> getAPageUserDesc(int startNo, int pageSize, String sortBy);
+    List<SmartbusUser> getAPageUserAsc(int startNo, int pageSize, String sortBy);
+    List<SmartbusUser> getAPageUser(int startNo, int pageSize, String sortBy, String direction);
+
+    List<SmartbusUser> getAPageUserByNameSearch(int startNo, int pageSize, String sortBy, String direction, String searchContent);
+    List<SmartbusUser> getAPageUserByCapacity(int startNo, int pageSize, String sortBy, String direction, int capacity);
+    List<SmartbusUser> getAPageUserByCompanySearch(int startNo, int pageSize, String sortBy, String direction, String searchContent);
+
+    Integer getUserNumByNameSearch(String searchContent);
+    Integer getUserNumByCapacity(int capacity);
+    Integer getUserNumByCompanySearch(String searchContent);
+
+}
